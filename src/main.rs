1084faf73c94a9e668280b2a3f3e200b929c7a45
Abhishek Singh <abhishek17513@gmail.com>
2024-12-12 18:55:52 +0530

    simple VM and compiler
    link: https://cstack.github.io/db_tutorial/parts/part2.html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    process,
+};
 
 type InputBuffer = String;
+
+pub enum SqliteErr {
+    UnrecognizedCommand,
+}
+
+pub enum StatementType {
+    INSERT,
+    SELECT,
+}
+
+pub struct Statement {
+    _type: StatementType,
+}
+
+impl Statement {
+    pub fn new(_type: StatementType) -> Self {
+        Self { _type }
+    }
+}
 
 fn main() {
     let mut input_buffer = InputBuffer::new();
@@ -8,11 +30,29 @@ fn main() {
         print_prompt();
         read_input(&mut input_buffer);
 
-        if input_buffer == ".exit" {
-            return;
-        } else {
-            println!("Unrecognized command '{}'.", input_buffer);
+        // TODO: implement parser combinator here, or regex instead of doing this
+        let buffer: Vec<char> = input_buffer.chars().collect();
+
+        if buffer[0] == '.' {
+            match do_meta_command(&input_buffer) {
+                Ok(_) => continue,
+                Err(_) => {
+                    println!("Unrecognized command '{}'.", input_buffer);
+                    continue;
+                }
+            }
         }
+
+        let statement = match prepare_statement(&input_buffer) {
+            Ok(statement) => statement,
+            Err(_) => {
+                println!("Unrecognized keyword at start of '{}'.", input_buffer);
+                continue;
+            }
+        };
+
+        execute_statement(&statement);
+        println!("Executed.");
     }
 }
 
@@ -29,4 +69,31 @@ fn read_input(input_buffer: &mut InputBuffer) {
 
     // Ignore trailing newline
     *input_buffer = input_buffer.trim_end().to_owned();
+}
+
+fn do_meta_command(input_buffer: &InputBuffer) -> Result<(), SqliteErr> {
+    if input_buffer == ".exit" {
+        process::exit(0);
+    } else {
+        Err(SqliteErr::UnrecognizedCommand)
+    }
+}
+
+fn prepare_statement(input_buffer: &InputBuffer) -> Result<Statement, SqliteErr> {
+    match input_buffer.to_lowercase() {
+        buffer if buffer.starts_with("select") => Ok(Statement::new(StatementType::SELECT)),
+        buffer if buffer.starts_with("insert") => Ok(Statement::new(StatementType::INSERT)),
+        _ => Err(SqliteErr::UnrecognizedCommand),
+    }
+}
+
+fn execute_statement(statement: &Statement) {
+    match statement._type {
+        StatementType::INSERT => {
+            println!("This is where we would do an insert.");
+        }
+        StatementType::SELECT => {
+            println!("This is where we would do a select.");
+        }
+    }
 }
